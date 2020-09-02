3dbf3ab29b5edb0136db2dc1808aa72e77b91223
Wafa <mansouriwafa@yahoo.com>
2020-09-02 11:50:09 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
+import imageInSrc from './imageInSrc.jpg';
 import './App.css';
+import './style.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
+      <h1 className="title red">Mansouri Wafa</h1>
+      <br/>
+      <img src={imageInSrc}/>
+      <br/>
+      <img src="/imageInPublic.jpg"/>  
+      </div>
+      <video width="320" height="240" controls>
+      <source src="myVideo.mp4" type="video/mp4"/> 
+      </video>
     </div>
   );
 }
